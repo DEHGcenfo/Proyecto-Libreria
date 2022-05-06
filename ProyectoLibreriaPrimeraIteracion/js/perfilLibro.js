3037629ead937b981e5db5a930d58fad5fb61f6c
unknown <leetrons21@gmail.com>
2022-05-06 01:47:53 -0600

    l

--- ProyectoLibreriaPrimeraIteracion/js/perfilLibro.js
+++ ProyectoLibreriaPrimeraIteracion/js/perfilLibro.js
@@ -22,12 +22,16 @@ const inicializar = async() => {
 const mostrarDatos = (libroEncontrado) => {
     seccionCartas.innerHTML = ''
 
+
     let carta = document.createElement('div')
     carta.classList.add('carta')
+
 
     let contInfo = document.createElement('section')
     contInfo.classList.add('contInfo')
 
+    let contTitulo = document.createElement('section')
+    contTitulo.classList.add('contTitulo')
 
     let divIzquierdo = document.createElement('div')
     divIzquierdo.classList.add('divIzquierdo')
@@ -35,17 +39,21 @@ const mostrarDatos = (libroEncontrado) => {
     let divDerecho = document.createElement('div')
     divDerecho.classList.add('divDerecho')
 
-    let titulo = document.createElement('a')
-    titulo.classList.add('titulo')
-    titulo.textContent = libroEncontrado.titulo
-
     let foto = document.createElement('img')
     foto.classList.add('fotoLibro')
     foto.src = libroEncontrado.foto
 
+    let titulo = document.createElement('h1')
+    titulo.classList.add('titulo')
+    titulo.textContent = libroEncontrado.titulo
 
+    let resenna = document.createElement('p')
+    resenna.classList.add('resenna')
+    resenna.textContent = `Reseña del libro: ${libroEncontrado.resenna}`
 
-
+    let isbn = document.createElement('p')
+    isbn.classList.add('isbn')
+    isbn.textContent = `ISBN: ${libroEncontrado.isbn}`
 
     let autor = document.createElement('p')
     autor.classList.add('autor')
@@ -53,30 +61,52 @@ const mostrarDatos = (libroEncontrado) => {
 
     let precio = document.createElement('p')
     precio.classList.add('precio')
-    precio.textContent = libroEncontrado.precio
-
-    let isbn = document.createElement('p')
-    isbn.classList.add('isbn')
-    isbn.textContent = `ISBN: ${libroEncontrado.isbn}`
+    precio.textContent = `Precio: ${libroEncontrado.precio}`
 
 
     let formato = document.createElement('p')
     formato.classList.add('formato')
     formato.textContent = `Formato: ${libroEncontrado.formato}`
 
+    let fechaPublicacion = document.createElement('p')
+    fechaPublicacion.classList.add('fechaPublicacion')
+    fechaPublicacion.textContent = `Fecha de Publicación: ${libroEncontrado.fechaPublicacion}`
 
-    carta.appendChild(titulo)
-    carta.appendChild(foto)
-    carta.appendChild(contInfo)
-    divIzquierdo.appendChild(autor)
-    divIzquierdo.appendChild(isbn)
-    divIzquierdo.appendChild(formato)
-    divDerecho.appendChild(precio)
+    let editorial = document.createElement('p')
+    editorial.classList.add('editorial')
+    editorial.textContent = `Editorial: ${libroEncontrado.editorial}`
+
+    let premios = document.createElement('p')
+    premios.classList.add('premios')
+    premios.textContent = `Premios: ${libroEncontrado.premios}`
+
+    let idioma = document.createElement('p')
+    idioma.classList.add('idioma')
+    idioma.textContent = `Idioma: ${libroEncontrado.idioma}`
+
+    let generosLiterarios = document.createElement('p')
+    generosLiterarios.classList.add('generosLiterarios')
+    generosLiterarios.textContent = `Generos literarios: ${libroEncontrado.generosLiterarios}`
+
+
+
+    carta.appendChild(contTitulo)
+    contTitulo.appendChild(titulo)
     contInfo.appendChild(divIzquierdo)
     contInfo.appendChild(divDerecho)
+    divIzquierdo.appendChild(foto)
+    divIzquierdo.appendChild(autor)
+    divIzquierdo.appendChild(precio)
+    divDerecho.appendChild(resenna)
+    divDerecho.appendChild(isbn)
+    divDerecho.appendChild(fechaPublicacion)
+    divDerecho.appendChild(editorial)
+    divDerecho.appendChild(premios)
+    divDerecho.appendChild(idioma)
+    divDerecho.appendChild(generosLiterarios)
+    divDerecho.appendChild(formato)
+    carta.appendChild(contInfo)
     seccionCartas.appendChild(carta)
-
-
 
 
 };
